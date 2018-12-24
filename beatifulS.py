a32eed98b1e8319776fde8655d2cb4c5dbbab14f
Алексей (Дом) <untouchable.infinity@gmail.com>
2018-12-24 22:17:10 +0300

    first commit

---
+++ beatifulS.py
@@ -0,0 +1,15 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+url = input('Enter - ')
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+##Retrive all anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href',None))
