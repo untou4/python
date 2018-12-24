f95da73f9220719084b1275acd6a377a4a993cb6
Алексей (Дом) <untouchable.infinity@gmail.com>
2018-12-24 23:19:11 +0300

    second assignment((

--- beautifilS2.py
+++ beautifilS2.py
@@ -2,25 +2,29 @@ import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
 
+
+class
+
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter - ')
+#url = input('Enter - ')
+#count = input('Enter count:')
+#position = input('Enter position')
+
+url = 'http://py4e-data.dr-chuck.net/known_by_Fikret.html'
+count = 4
+position = 3
+
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 
 # Retrive all anchor tags
 tags = soup('span') #<a....>...</a>
-s = 0
-c = 0
 for tag in tags:
     s+=int(tag.contents[0])
     c+=1
-    print(tag)
-    print(tag.get('href', None))
-    print(tag.contents[0])
-    print(tag.attrs)
 print('Count ', c)
 print('Sum ', s)
